60aeebf2a811d9ac4880a181c1bd231a574a9757
XDasdazdea <50545507+Glubus@users.noreply.github.com>
2025-02-23 17:48:46 +0100

    Hotfix : could not rate with rate_map

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ pub use osu::*;
 use std::path::{Path, PathBuf};
 
 
-fn rate_map(osu_file_path: &str, rate : f32) -> eyre::Result<()>{
+pub fn rate_map(osu_file_path: &str, rate : f32) -> eyre::Result<()>{
     let mut map: Beatmap = rosu_map::Beatmap::from_path(osu_file_path)?;
     let audio_path = map.audio_file.clone();
     let audio_output_path = PathBuf::from(format!(
@@ -20,14 +20,16 @@ fn rate_map(osu_file_path: &str, rate : f32) -> eyre::Result<()>{
             .ok_or_else(|| eyre::eyre!("Invalid audio file name"))?,
         rate
     )).to_str().unwrap().to_string();
-
-    change_osu_speed(&mut map, osu_file_path, 1.5, &audio_output_path)?;
+    println!("Audio output path: {}", audio_output_path);
+    change_osu_speed(&mut map, osu_file_path, rate, &audio_output_path)?;
 
 
     let osu_file_dir = Path::new(&osu_file_path)
         .parent()
         .ok_or_else(|| eyre::eyre!("Invalid osu file directory"))?;
-    let audio_path = osu_file_dir.join(&map.audio_file);
+    println!("Osu file directory: {}", osu_file_dir.to_str().unwrap());
+    let audio_path = osu_file_dir.join(audio_path);
+    println!("Audio path: {}", audio_path.to_str().unwrap());
     let audio_output_path = audio_path.with_file_name(format!(
         "{}_{}.ogg",
         audio_path
@@ -41,3 +43,26 @@ fn rate_map(osu_file_path: &str, rate : f32) -> eyre::Result<()>{
     Ok(())
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_rate_map_different_speeds() -> eyre::Result<()> {
+        // Premier test avec une vitesse de 1.5
+        rate_map("./resources/test.osu", 1.5)?;
+
+        // Vérification que les fichiers de sortie existent
+        assert!(Path::new("./resources/test_1.5x.osu").exists());
+        assert!(Path::new("./resources/decoy_1.5.ogg").exists());
+
+        // Deuxième test avec une vitesse de 0.75
+        rate_map("./resources/test.osu", 0.75)?;
+
+        // Vérification que les fichiers de sortie existent
+        assert!(Path::new("./resources/test_0.75x.osu").exists());
+        assert!(Path::new("./resources/decoy_0.75.ogg").exists());
+
+        Ok(())
+    }
+}
